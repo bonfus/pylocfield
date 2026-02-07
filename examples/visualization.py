d433bd0d0e4f62902ff7f3adc850cfb8dd1a211e
Pietro <bonfus@gmail.com>
2026-02-07 20:12:04 +0100

    Two options for visualization (and a bit of refactoring)

--- examples/visualization.py
+++ examples/visualization.py
@@ -4,12 +4,7 @@ from pylocfield.realspace import compute_magnetic_moments
 from ase.atoms import Atoms
 
 
-def save_mcif(label: str, atoms: Atoms, supercell_matrix: np.array):
-    """
-    This function assumes that one q is specified for
-    each pair of (+q, -q) propagation vectors.
-    """
-    from pymatgen.core import Structure
+def generate_supercells_with_moments(atoms: Atoms, supercell_matrix: np.array):
     from ase.build.supercells import make_supercell
 
     na = len(atoms)
@@ -24,18 +19,44 @@ def save_mcif(label: str, atoms: Atoms, supercell_matrix: np.array):
 
     # Check that the same happens if we use the second atom as reference
     if na > 1:
-        assert(np.allclose(
+        assert np.allclose(
             cell.cartesian_positions(
                 np.floor(cell.scaled_positions(sc.positions[1::na]))
             ),
             R,
-        ))
+        )
 
+    magnetic_structures = []
     for i, q in enumerate(qs):
         mom = compute_magnetic_moments(R, q, fcs[:, i, :]).reshape(-1, 3)
 
-        sc.set_initial_magnetic_moments(None)
-        sc.set_initial_magnetic_moments(mom)
+        scm = sc.copy()
+        scm.set_initial_magnetic_moments(None)
+        scm.set_initial_magnetic_moments(mom)
 
+        magnetic_structures.append(scm)
+
+    return magnetic_structures
+
+
+def save_mcif(label: str, atoms: Atoms, supercell_matrix: np.array):
+    """
+    This function assumes that one q is specified for
+    each pair of (+q, -q) propagation vectors.
+    """
+    from pymatgen.core import Structure
+
+    for i, sc in enumerate(generate_supercells_with_moments(atoms, supercell_matrix)):
         st = Structure.from_ase_atoms(sc)
         st.to(f"{label}-{st.chemical_system}-q{i + 1}.mcif")
+
+
+def show_ase(atoms: Atoms, supercell_matrix: np.array, gui: bool = False, **kwargs):
+
+    if gui:
+        from ase.visualize import view as show
+    else:
+        from ase.visualize.plot import plot_atoms as show
+
+    for i, sc in enumerate(generate_supercells_with_moments(atoms, supercell_matrix)):
+        show(sc, **kwargs)
